957921cd325d900f320558167f71bfccf82be6cd
Krzysztof Chromiec <krzysztof.chromiec1@gmail.com>
2020-09-24 17:51:26 +0200

    initial commit

---
+++ src/main/java/com/codecool/fusy_qs/FusyQsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.fusy_qs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusyQsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusyQsApplication.class, args);
+	}
+
+}
